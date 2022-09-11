86fe4a9cc939818ce3f93b0c71e0251f6aecb7d0
hanh-do <hoghanhdo@gmail.com>
2022-09-11 16:54:21 +0900

    rotate array

---
+++ easy/rotateArray.php
@@ -0,0 +1,48 @@
+<?php
+class Solution {
+    /**
+     * @var int $k
+     * $@var int[] $arr
+     * @return int[] $rotated
+     */
+    public function rotateArray(array $nums, int $k) {
+        // array that need to rotate 
+        // ------->-->
+        // array after rotation
+        // -->-------->
+        if (empty($nums) || count($nums) < 2) {
+            return null;
+        }
+        $n = count($nums);
+        $times = $k % $n;
+
+        // revers entire array
+        // <--<--------
+        $nums = $this->reverse($nums, 0, $n-1);
+
+        // reverse the part that need rotation
+        // --><---------
+        $nums = $this->reverse($nums, 0, $times-1);
+
+        // reverse the remaining part of original array back
+        // -->---------->
+        $nums = $this->reverse($nums, $times, $n-1);
+
+        return $nums;
+    }
+
+    public function reverse(array $nums, int $start, int $end) {
+        while($start < $end) {
+            $tmp = $nums[$start];
+            $nums[$start] = $nums[$end];
+            $nums[$end] = $tmp;
+            $start++;
+            $end--;
+        }
+        return $nums;
+    }
+}
+
+$solution = new Solution();
+$rotatedArr = $solution->rotateArray([1,2,3,4,6,7,8], 3);
+print_r($rotatedArr);
